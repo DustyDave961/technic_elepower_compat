5b58cb6c10ff0556931a14f54e62cc4f16c0ada8
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2023-09-12 14:35:29 -0500

    Update register_alloys.lua
    Recipes added and organized.

--- alloy_compat/register_alloys.lua
+++ alloy_compat/register_alloys.lua
@@ -1,7 +1,8 @@
 --Elepower Recipes for Technic Furnaces
 technic.register_alloy_recipe({
-	input  = {"elepower_dynamics:iron_ingot 3", "elepower_dynamics:nickel_ingot"},
-	output = "elepower_dynamics:invar_ingot 4",
+	input  = {"default:copper_ingot 2", "moreores:silver_ingot"},
+	output = "basic_materials:brass_ingot 3",
+	time   = 8
 })
 
 technic.register_alloy_recipe({
@@ -10,19 +11,8 @@ technic.register_alloy_recipe({
 })
 
 technic.register_alloy_recipe({
-	input  = {"basic_materials:silicon", "elepower_dynamics:coal_dust 2"},
-	output = "elepower_dynamics:silicon_wafer",
-})
-
-technic.register_alloy_recipe({
 	input  = {"default:coal_lump", "elepower_dynamics:coal_dust 4"},
 	output = "elepower_dynamics:graphite_ingot",
-})
-
-technic.register_alloy_recipe({
-	input  = {"elepower_dynamics:silicon_wafer", "elepower_dynamics:gold_dust 4"},
-	output = "elepower_dynamics:silicon_wafer_doped",
-	time   = 8
 })
 
 technic.register_alloy_recipe({
@@ -32,22 +22,51 @@ technic.register_alloy_recipe({
 })
 
 technic.register_alloy_recipe({
-	input  = {"default:copper_ingot 2", "moreores:silver_ingot"},
-	output = "basic_materials:brass_ingot 3",
-	time   = 8
+	input  = {"default:bronze_ingot", "default:steel_ingot 4"},
+	output = "elepower_machines:heat_casing 4",
 })
 
 technic.register_alloy_recipe({
-	input  = {"default:bronze_ingot", "default:steel_ingot 4"},
-	output = "elepower_machines:heat_casing 4",
+	input  = {"elepower_dynamics:iron_ingot 3", "elepower_dynamics:nickel_ingot"},
+	output = "elepower_dynamics:invar_ingot 4",
+})
+
+technic.register_alloy_recipe({
+	input  = {"basic_materials:silicon", "elepower_dynamics:coal_dust 2"},
+	output = "elepower_dynamics:silicon_wafer",
+})
+
+technic.register_alloy_recipe({
+	input  = {"elepower_dynamics:silicon_wafer", "elepower_dynamics:gold_dust 4"},
+	output = "elepower_dynamics:silicon_wafer_doped",
+	time   = 8
 })
 
 --Technic Recipes for Elepower Furnaces
 elepm.register_craft({
 	type   = "alloy",
+	recipe = {"technic:copper_dust 2", "technic:zinc_dust"},
+	output = "technic:brass_dust 3",
+})
+
+elepm.register_craft({
+	type   = "alloy",
+	recipe = {"default:copper_ingot 2", "technic:zinc_ingot"},
+	output = "basic_materials:brass_ingot 3",
+})
+
+elepm.register_craft({
+	type   = "alloy",
 	recipe = {"technic:copper_dust 7", "technic:tin_dust"},
 	output = "technic:bronze_dust 8",
 	time   = 8
+})
+
+elepm.register_craft({
+	type   = "alloy",
+	recipe = {"default:ice", "bucket:bucket_empty"},
+	output = "bucket:bucket_water",
+	time   = 1
 })
 
 elepm.register_craft({
@@ -76,28 +95,9 @@ elepm.register_craft({
 
 elepm.register_craft({
 	type   = "alloy",
-	recipe = {"technic:carbon_steel_dust 4", "technic:chromium_dust"},
-	output = "technic:stainless_steel_dust 5",
-	time   = 5
-})
-
-elepm.register_craft({
-	type   = "alloy",
-	recipe = {"technic:carbon_steel_ingot 4", "technic:chromium_ingot"},
-	output = "technic:stainless_steel_ingot 5",
-	time   = 5
-})
-
-elepm.register_craft({
-	type   = "alloy",
-	recipe = {"technic:copper_dust 2", "technic:zinc_dust"},
-	output = "technic:brass_dust 3",
-})
-
-elepm.register_craft({
-	type   = "alloy",
-	recipe = {"default:copper_ingot 2", "technic:zinc_ingot"},
-	output = "basic_materials:brass_ingot 3",
+	recipe = {"technic:raw_latex 4", "technic:coal_dust 2"},
+	output = "technic:rubber 6",
+	time   = 1
 })
 
 elepm.register_craft({
@@ -114,14 +114,40 @@ elepm.register_craft({
 
 elepm.register_craft({
 	type   = "alloy",
-	recipe = {"technic:raw_latex 4", "technic:coal_dust 2"},
-	output = "technic:rubber 6",
-	time   = 1
+	recipe = {"technic:carbon_steel_dust 4", "technic:chromium_dust"},
+	output = "technic:stainless_steel_dust 5",
+	time   = 5
 })
 
 elepm.register_craft({
 	type   = "alloy",
-	recipe = {"default:ice", "bucket:bucket_empty"},
-	output = "bucket:bucket_water",
-	time   = 1
+	recipe = {"technic:carbon_steel_ingot 4", "technic:chromium_ingot"},
+	output = "technic:stainless_steel_ingot 5",
+	time   = 5
 })
+
+--New Recipes
+---Elepower Silicon from Technic Dust
+elepm.register_craft({
+	type   = "alloy",
+	recipe = {"default:desert_sand 2", "technic:coal_dust 2"},
+	output = "elepower_dynamics:silicon_wafer",
+})
+
+elepm.register_craft({
+	type   = "alloy",
+	recipe = {"basic_materials:silicon", "technic:coal_dust 2"},
+	output = "elepower_dynamics:silicon_wafer",
+})
+
+technic.register_alloy_recipe({
+	input  = {"default:desert_sand 2", "technic:coal_dust 2"},
+	output = "elepower_dynamics:silicon_wafer",
+})
+
+technic.register_alloy_recipe({
+	input  = {"basic_materials:silicon", "technic:coal_dust 2"},
+	output = "elepower_dynamics:silicon_wafer",
+})
+
+---Technic Silicon from Elepower Dust
