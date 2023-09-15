a586e0de6017e7fe6f216c757f97e7e98c0ab571
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2023-09-15 15:51:10 -0500

    Update grind_recipes.lua
    Recipe function added.

--- grind_compat/grind_recipes.lua
+++ grind_compat/grind_recipes.lua
@@ -46,7 +46,7 @@ elepm.register_craft({
 	type   = "grind",
 	recipe = {"technic:chromium_lump"},
 	output = "technic:chromium_dust 2",
-	time   = 8
+	time   = 6
 })
 
 elepm.register_craft({
@@ -60,17 +60,17 @@ elepm.register_craft({
 	type   = "grind",
 	recipe = {"technic:lead_lump"},
 	output = "technic:lead_dust 2",
-	time   = 8
-})
-
-elepm.register_craft({
-	type   = "grind",
-	recipe = {"default:desert_sandstone"},
-	output = "default:desert_sand 2",
-	time   = 8
+	time   = 6
 })
 
 --Broken Sandstone recipes. Creates 4 sand instead of 2.
+--elepm.register_craft({
+	--type   = "grind",
+	--recipe = {"default:desert_sandstone"},
+	--output = "default:desert_sand 2",
+	--time   = 8
+--})
+
 --elepm.register_craft({
 	--type   = "grind",
 	--recipe = {"default:sandstone"},
@@ -103,35 +103,26 @@ elepm.register_craft({
 	type   = "grind",
 	recipe = {"technic:sulfur_lump"},
 	output = "technic:sulfur_dust 2",
-	time   = 8
+	time   = 6
 })
 
 elepm.register_craft({
 	type   = "grind",
 	recipe = {"technic:zinc_lump"},
 	output = "technic:zinc_dust 2",
-	time   = 8
+	time   = 6
 })
+
+register_grind_recipe("stairs:slab_cobble 2", "default:gravel", 3, 5)
 
 --Uranium Recipes for Both
 elepm.register_craft({
 	type   = "grind",
 	recipe = {"technic:uranium_lump"},
 	output = "technic:uranium_dust 2",
-	time   = 8
+	time   = 6
 })
 
 if minetest.get_modpath("elepower_nuclear") then
-	technic.register_grinder_recipe({
-		input  = {"elepower_nuclear:uranium_lump"},
-		output = "technic:uranium_dust 2",
-		time   = 3
-	})
-
-	elepm.register_craft({
-		type   = "grind",
-		recipe = {"elepower_nuclear:uranium_lump"},
-		output = "technic:uranium_dust 2",
-		time   = 8
-	})
+	register_grind_recipe("elepower_nuclear:uranium_lump", "technic:uranium_dust 2", 3, 6)
 end
