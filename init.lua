67590febf8cc31182068753b1abea65f88fe8cc1
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2023-09-15 21:41:39 -0500

    Update init.lua
    Modpath fixed.

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local modpath = minetest.get_modpath("alloy_compat")
+local modpath = minetest.get_modpath("technic_elepower_compat")
 
 dofile(modpath .. "/functions.lua")
 dofile(modpath .. "/grind_recipes.lua")
