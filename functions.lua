9b5e8f7dd45d288b28cb6283da8da836bfe558b7
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2023-09-15 21:44:53 -0500

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -1,5 +1,19 @@
---This is a function that registers grinding recipes for technic grinders and elepower pulverizers.
+--Register alloying recipes for both technic and elepower alloy furnaces.
+function register_alloy_recipe(input1, input2, output1, time1, time2) 
+	technic.register_alloy_recipe({
+		input  = {input1, input2},
+		output = output1,	
+		time = time1 or 6
+	})
+	elepm.register_craft({
+		type   = "alloy",
+		recipe = {input1, input2},
+		output = output1,
+		time = time2 or 4
+	})
+end
 
+--Register grinding recipes for elepower pulverizers, grindstones, and technic grinders.
 function register_grind_recipe(input1, output1, time1, time2) 
 	technic.register_grinder_recipe({
 		input  = {input1},
@@ -14,9 +28,4 @@ function register_grind_recipe(input1, output1, time1, time2)
 	})
 end
 
-minetest.chat_send_all("The function is being called.")
-
---For grinding ore materials in elepower grindstones and pulverizers, time = output + 4.
-
-
-
+---For grinding ore materials in elepower grindstones and pulverizers, time = output + 4.
