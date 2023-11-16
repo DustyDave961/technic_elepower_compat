de185257b9dcfae2f4f9b435eb9bde68f3928cf0
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2023-11-15 19:32:33 -0600

    Update init.lua
    Removed: functions.lua Added: overrides.lua and craft_compat.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local modpath = minetest.get_modpath("technic_elepower_compat")
 
-dofile(modpath .. "/functions.lua")
+dofile(modpath .. "/overrides.lua")
 dofile(modpath .. "/grind_recipes.lua")
 dofile(modpath .. "/register_alloys.lua")
 dofile(modpath .. "/ore_compat.lua")
+dofile(modpath .. "/craft_compat.lua")
