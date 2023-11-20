0cec349254a96482ea896595223e7c1f0dc50209
DustyBagel <150477174+DustyBagel@users.noreply.github.com>
2023-11-20 16:41:00 -0600

    Update init.lua
    if setting is enabled add the power converter

--- init.lua
+++ init.lua
@@ -4,3 +4,7 @@ dofile(modpath .. "/overrides.lua")
 dofile(modpath .. "/grind_recipes.lua")
 dofile(modpath .. "/register_alloys.lua")
 dofile(modpath .. "/craft_compat.lua")
+
+if minetest.get_bool("enable_power_converter") then
+  dofile(modpath .. "/power_converter.lua")
+end
