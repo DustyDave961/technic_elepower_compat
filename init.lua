d97b47b616c2affc841146c1df5c2df02768ddf1
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2023-11-22 00:59:49 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local modpath = minetest.get_modpath("technic_elepower_compat")
 
 dofile(modpath .. "/overrides.lua")
 dofile(modpath .. "/grind_recipes.lua")
-dofile(modpath .. "/register_alloys.lua")
+dofile(modpath .. "/alloy_compat.lua")
 dofile(modpath .. "/craft_compat.lua")
 
 if minetest.get_setting("enable_power_converter") then
