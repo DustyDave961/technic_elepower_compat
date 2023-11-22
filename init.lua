e3036178d021427600b0eaed5cb7eef63771ae59
DustyBagel <150477174+DustyBagel@users.noreply.github.com>
2023-11-22 00:08:28 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -8,3 +8,10 @@ dofile(modpath .. "/craft_compat.lua")
 if minetest.get_setting("enable_power_converter") then
   dofile(modpath .. "/power_converter.lua")
 end
+
+if minetest.get_setting("enable_on_join_player_message") then
+    minetest.register_on_joinplayer(function(player)
+        local name = player:get_player_name()
+        minetest.chat_send_player(name, "If you would like to test the beta power converter node, then go to settings, search 'technic_elepower_compat', and enable the converter.")
+    end)
+end
