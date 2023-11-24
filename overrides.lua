43ea662a68865044027a271abf2efe8394864977
DustyBagel <150477174+DustyBagel@users.noreply.github.com>
2023-11-23 19:44:42 -0600

    Add settings for what lead and zinc to use to overrides.lua

--- overrides.lua
+++ overrides.lua
@@ -1,21 +1,51 @@
 --Ore overrides to clear unneeded ores.
-minetest.register_abm({
-    nodenames = {"elepower_dynamics:stone_with_lead"}, -- replace with the name of the ore
-    interval = 1, -- runs every 1 second
-    chance = 1, -- always fires
-    action = function(pos)
-        minetest.swap_node(pos, {name = "default:stone"})
-    end,
-})
+local lead_to_use = minetest.settings:get("lead_used")
 
-minetest.register_abm({
-    nodenames = {"elepower_dynamics:stone_with_zinc"}, -- replace with the name of the ore
-    interval = 1, -- runs every 1 second
-    chance = 1, -- always fires
-    action = function(pos)
-        minetest.swap_node(pos, {name = "default:stone"})
-    end,
-})
+if lead_to_use == "technic" then
+    minetest.register_abm({
+        nodenames = {"elepower_dynamics:stone_with_lead"}, -- replace with the name of the ore
+        interval = 1, -- runs every 1 second
+        chance = 1, -- always fires
+        action = function(pos)
+            minetest.swap_node(pos, {name = "default:stone"})
+        end,
+    })
+elseif lead_to_use == "elepower" then
+    minetest.register_abm({
+        nodenames = {"technic:mineral_lead"}, -- replace with the name of the ore
+	interval = 1, -- runs every 1 second
+    	chance = 1, -- always fires
+  	action = function(pos)
+        	minetest.swap_node(pos, {name = "default:stone"})
+    	end,
+    })
+elseif lead_to_use == "both" then
+	return false
+end
+
+local zinc_to_use = minetest.settings:get("zinc_used")
+
+if zinc_to_use == "technic" then
+    minetest.register_abm({
+    	nodenames = {"elepower_dynamics:stone_with_zinc"}, -- replace with the name of the ore
+    	interval = 1, -- runs every 1 second
+    	chance = 1, -- always fires
+    	action = function(pos)
+        	minetest.swap_node(pos, {name = "default:stone"})
+    	end,
+    })
+elseif zinc_to_use == "elepower" then 
+    minetest.register_abm({
+        nodenames = {"technic:mineral_zinc"}, -- replace with the name of the ore
+	interval = 1, -- runs every 1 second
+    	chance = 1, -- always fires
+  	action = function(pos)
+        	minetest.swap_node(pos, {name = "default:stone"})
+    	end,
+    })
+elseif zinc_to_use == "both" then
+    return false
+end
 
 --Adding dust and ingot groups
 minetest.override_item("technic:coal_dust", {
