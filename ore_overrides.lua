616f2a44fbe76023e0c84592db4c8d0e664ca2cd
DustyDave961 <144002335+DustyDave961@users.noreply.github.com>
2024-11-25 15:19:35 -0600

    Update ore_overrides.lua
    Minetest is Luanti now, so "core" table should be referenced instead of "minetest".

--- ore_overrides.lua
+++ ore_overrides.lua
@@ -1,46 +1,46 @@
 --Ore overrides to clear unneeded ores.
-local lead_to_use = minetest.settings:get("lead_used") or "both"
+local lead_to_use = core.settings:get("lead_used") or "both"
 
 if lead_to_use == "technic" then
-    minetest.register_abm({
+    core.register_abm({
         nodenames = {"elepower_dynamics:stone_with_lead"}, -- replace with the name of the ore
         interval = 1, -- runs every 1 second
         chance = 1, -- always fires
         action = function(pos)
-            minetest.swap_node(pos, {name = "default:stone"})
+            core.swap_node(pos, {name = "default:stone"})
         end,
     })
 elseif lead_to_use == "elepower" then
-    minetest.register_abm({
+    core.register_abm({
         nodenames = {"technic:mineral_lead"}, -- replace with the name of the ore
 		interval = 1, -- runs every 1 second
     	chance = 1, -- always fires
 		action = function(pos)
-        	minetest.swap_node(pos, {name = "default:stone"})
+        	core.swap_node(pos, {name = "default:stone"})
     	end,
     })
 elseif lead_to_use == "both" then
 	return false
 end
 
-local zinc_to_use = minetest.settings:get("zinc_used") or "both"
+local zinc_to_use = core.settings:get("zinc_used") or "both"
 
 if zinc_to_use == "technic" then
-    minetest.register_abm({
+    core.register_abm({
     	nodenames = {"elepower_dynamics:stone_with_zinc"}, -- replace with the name of the ore
     	interval = 1, -- runs every 1 second
     	chance = 1, -- always fires
     	action = function(pos)
-        	minetest.swap_node(pos, {name = "default:stone"})
+        	core.swap_node(pos, {name = "default:stone"})
     	end,
     })
 elseif zinc_to_use == "elepower" then 
-    minetest.register_abm({
+    core.register_abm({
         nodenames = {"technic:mineral_zinc"}, -- replace with the name of the ore
 	interval = 1, -- runs every 1 second
     	chance = 1, -- always fires
   	action = function(pos)
-        	minetest.swap_node(pos, {name = "default:stone"})
+        	core.swap_node(pos, {name = "default:stone"})
     	end,
     })
 elseif zinc_to_use == "both" then
