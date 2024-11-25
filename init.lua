2830bdfcfd708f634d77e61741a2dc3db668c21d
DustyDave961 <144002335+DustyDave961@users.noreply.github.com>
2024-11-25 15:50:42 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local modpath = minetest.get_modpath("technic_elepower_compat")
+local modpath = core.get_modpath("technic_elepower_compat")
 
 dofile(modpath .. "/overrides.lua")
 dofile(modpath .. "/grind_recipes.lua")
@@ -8,12 +8,12 @@ dofile(modpath .. "/compress_compat.lua")
 dofile(modpath .. "/manual.lua")
 dofile(modpath .. "/ore_overrides.lua")
 
-if minetest.settings:get_bool("enable_power_converter") then
+if core.settings:get_bool("enable_power_converter") then
   dofile(modpath .. "/power_converter.lua")
 end
 
-if minetest.settings:get_bool("enable_on_join_player_message") then
-    minetest.register_on_newplayer(function(player)
-        minetest.chat_send_player(player:get_player_name(), "[Technic Elepower Compat] If you would like to test the beta power converter node, then go to settings, search 'technic_elepower_compat', and enable the converter.")
+if core.settings:get_bool("enable_on_join_player_message") then
+    core.register_on_newplayer(function(player)
+        core.chat_send_player(player:get_player_name(), "[Technic Elepower Compat] If you would like to test the beta power converter node, then go to settings, search 'technic_elepower_compat', and enable the converter.")
     end)
 end
